1d191315f0a1ad7a9ddca8f2d45aa9b2b54526e5
philippch <philipp@philippchristoph.de>
2024-05-29 11:38:47 +0200

    Update splat dynamically.

--- src/app/components/luma-canvas/luma-canvas.component.ts
+++ src/app/components/luma-canvas/luma-canvas.component.ts
@@ -1,27 +1,62 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {PerspectiveCamera, Scene, WebGLRenderer} from 'three';
+import {AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
+import {PerspectiveCamera, Plane, PlaneHelper, Scene, Vector3, WebGLRenderer} from 'three';
 import {LumaSplatsThree} from '@lumaai/luma-web';
 import {LookingGlassWebXrService} from "../../services/looking-glass-web-xr.service";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import {VRButton} from "three/examples/jsm/webxr/VRButton";
+import {FormsModule} from "@angular/forms";
+import {ThreeMFLoader} from "three/examples/jsm/loaders/3MFLoader";
 
 @Component({
     selector: 'luma-canvas',
     standalone: true,
-    imports: [],
+    imports: [
+        FormsModule
+    ],
     templateUrl: './luma-canvas.component.html',
     styleUrl: './luma-canvas.component.css'
 })
-export class LumaCanvasComponent implements AfterViewInit {
+export class LumaCanvasComponent implements AfterViewInit, OnChanges {
+
+    @Input()
+    public showFocusPlane = true;
 
     @ViewChild('lumaCanvas')
-    private lumaCanvas!: ElementRef<HTMLCanvasElement>;
+    public lumaCanvas!: ElementRef<HTMLCanvasElement>;
+
+    public targetZ = 0;
+
+    private _scene = new Scene();
+    private _splat?: LumaSplatsThree;
+    private _focusPlane = this.createFocusPlane();
 
     constructor(private webXrService: LookingGlassWebXrService) {
     }
 
-    ngAfterViewInit() {
+    public ngAfterViewInit() {
         this.webXrService.createPolyfill();
         this.createRenderer();
+    }
+
+    public ngOnChanges(changes: SimpleChanges): void {
+        if (changes['showFocusPlane']) {
+            this.onShowFocusPlane(this.showFocusPlane);
+        }
+    }
+
+    public onTargetZChange(value: number) {
+        this.webXrService.update({targetX: value});
+    }
+
+    public loadSplat(url: string) {
+        if (this._splat) {
+            this._scene.remove(this._splat);
+        }
+
+        this._splat = new LumaSplatsThree({
+            source: url
+        });
+        this._scene.add(this._splat);
     }
 
     private createRenderer() {
@@ -33,21 +68,33 @@ export class LumaCanvasComponent implements AfterViewInit {
         renderer.setSize(window.innerWidth, window.innerHeight, false);
         renderer.xr.enabled = true;
 
-        const scene = new Scene();
+        const lumaCanvas = this.lumaCanvas;
+
+        setTimeout(function () {
+            renderer.xr.enabled = true
+            lumaCanvas.nativeElement.insertAdjacentElement('afterend', VRButton.createButton(renderer))
+        }, 2000)
+
         const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         camera.position.z = 2;
 
         const controls = new OrbitControls(camera, this.lumaCanvas.nativeElement);
         controls.enableDamping = true;
 
-        const splat = new LumaSplatsThree({
-            source: 'https://lumalabs.ai/capture/d80d4876-cf71-4b8a-8b5b-49ffac44cd4a'
-        });
-        scene.add(splat);
-
         renderer.setAnimationLoop(() => {
             controls.update();
-            renderer.render(scene, camera);
+            renderer.render(this._scene, camera);
         });
+    }
+
+    private createFocusPlane(): PlaneHelper {
+        const plane = new Plane(new Vector3(1, 1, 0.2), 3);
+        return new PlaneHelper(plane, 1, 0xffff00);
+    }
+
+    private onShowFocusPlane(enabled: boolean) {
+        if (enabled) {
+
+        }
     }
 }
--- src/app/services/looking-glass-web-xr.service.ts
+++ src/app/services/looking-glass-web-xr.service.ts
@@ -1,18 +1,27 @@
 import {Injectable} from '@angular/core';
+// @ts-ignore: For some reason, this import isn't working properly when using auto-generated imports.
+// This is the proper import statement as suggested by Bryan from Looking Glass Factory.
 import {LookingGlassWebXRPolyfill} from "@lookingglass/webxr";
+import {ViewControlArgs} from "@lookingglass/webxr/dist/LookingGlassConfig";
 
 @Injectable({
     providedIn: 'root'
 })
 export class LookingGlassWebXrService {
+
+    private polyfill?: LookingGlassWebXRPolyfill;
+
     public createPolyfill() {
-        LookingGlassWebXRPolyfill.init({
-            //quiltResolution: 8,
+        this.polyfill = new LookingGlassWebXRPolyfill({
             numViews: 45,
             targetY: 1,
-            targetZ: 0,
+            targetZ: -0.5, // This is the value we need to change to move the focus back and forth. We can also dynamically change these using the updat() method.
             targetDiam: 3,
             fovy: (14 * Math.PI) / 180,
         });
+    }
+
+    public update(config: Partial<ViewControlArgs>) {
+        this.polyfill.update(config);
     }
 }
